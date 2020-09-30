f2f3213a400b07fb11f7a9fbba40e54a222c2f31
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-30 19:39:20 +0300

    Removed Response from Employee controller

--- rest/src/main/java/com/example/rest/EmployeeController.java
+++ rest/src/main/java/com/example/rest/EmployeeController.java
@@ -7,7 +7,6 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
-import org.apache.catalina.connector.Response;
 import org.springframework.hateoas.CollectionModel;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -72,7 +71,9 @@ class EmployeeController {
     }
 
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }

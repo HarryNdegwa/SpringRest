07e2b4deffce06f1986e711fc29bac77453bc68b
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-30 17:27:22 +0300

    Added employee model assembler

--- rest/src/main/java/com/example/rest/EmployeeController.java
+++ rest/src/main/java/com/example/rest/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.example.rest;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
